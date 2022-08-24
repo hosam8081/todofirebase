49031f2f3e39c9498b69f83f3a8e5674c68fbd61
hosam8081 <hosamsalah8081@gmail.com>
2022-08-24 20:00:33 +0200

    edit home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Error from "./pages/Error";
 import PrivateRoute from "./components/PrivateRoute";
 import { useGlobalContext } from "./context/AuthContext";
 function App() {
-  const {darkMode} = useGlobalContext()
+  const { darkMode } = useGlobalContext();
   return (
     <>
-    <div className={`h-screen ${darkMode && "bg-black-1"}`}>
       <Router>
         <Routes>
           <Route element={<PrivateRoute />}>
-            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<Home />} />
           </Route>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/signup" element={<Signup />}/>
-          <Route path="*" element={<Error />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
-      </div>
     </>
   );
 }
